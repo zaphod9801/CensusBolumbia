61a0bb64345465c4c3df918d2f87e0c382aa7179
zaphod9801 <johanruiz932@gmail.com>
2021-11-22 16:13:15 -0500

    postgre sql

--- CensusBolumbia/settings.py
+++ CensusBolumbia/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
